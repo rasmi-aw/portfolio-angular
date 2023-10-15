a05f5487ac693b44e6fb8dc4a88c9c9e85c3356d
rasmi-aw <abdelwadoudrasmi@gmail.com>
2023-10-15 19:06:42 +0200

    update

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  fullname = "Fullname"
+  email = "Email"
+  content = "Content"
+
+  ngOnInit(): void {
+
+  }
 
 }
