209d929e3c5ea2e783e4811d9a1245061fbe5a76
rasmi-aw <abdelwadoudrasmi@gmail.com>
2023-10-15 18:32:05 +0200

    update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-  name :string = "Abdelouadoud Rasmi"
+export class HeaderComponent implements OnInit {
+  name: string | undefined;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.name = "Abdelouadoud Rasmi"
+  }
+
+  descriptionClicker($event: any) {
+    alert("description card clicked")
+    console.log("description card clicked");
+    if ($event) {
+      console.log($event.target);
+      console.log($event.target.value);
+    }
+  }
+
 }
