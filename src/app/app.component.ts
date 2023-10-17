f12bb0e8aa0683924a70e9c65c763442649ef738
rasmi-aw <abdelwadoudrasmi@gmail.com>
2023-10-17 11:01:49 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Portfolio} from "./portfolio";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Protfolio';
+  portfolio: Portfolio | undefined;
+
+  ngOnInit() {
+    this.portfolio = {
+      fullname: "Abdelouadoud Rasmi",
+      job: "Java developer",
+      description: "",
+      projects: []
+    }
+  }
 }
---
+++ src/app/portfolio.ts
@@ -0,0 +1,6 @@
+export interface Portfolio {
+  fullname: string
+  job: string
+  description: string
+  projects: []
+}
