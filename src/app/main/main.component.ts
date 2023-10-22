01fa3458e0c1cee20cf3da1faefd164f7b2c41c5
rasmi-aw <abdelwadoudrasmi@gmail.com>
2023-10-22 11:43:31 +0200

    update trackby

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -12,6 +12,7 @@ export class MainComponent {
   public getProjects(): Project[] {
     return [
       {
+        id: 1,
         featured: true,
         name: "Project name",
         date: "October 10th, 2023",
@@ -19,6 +20,7 @@ export class MainComponent {
         img: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80"
       },
       {
+        id: 2,
         featured: true,
         name: "Project name",
         date: "October 10th, 2023",
@@ -26,6 +28,7 @@ export class MainComponent {
         img: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80"
       },
       {
+        id: 3,
         featured: true,
         name: "Project name",
         date: "October 10th, 2023",
@@ -33,6 +36,10 @@ export class MainComponent {
         img: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80"
       }
     ]
+  }
+
+  trackByData(index: number, projects: any): number {
+    return projects.id;
   }
 }
 
