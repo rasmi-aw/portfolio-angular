00d2bdeaea1dfac1a207a89e885af5336db4d49c
rasmi-aw <abdelwadoudrasmi@gmail.com>
2023-10-23 15:36:42 +0200

    Update

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,18 +1,24 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Project} from "../project";
 import {ProjectSTATE} from "../project-state";
+import {ajax} from "rxjs/internal/ajax/ajax";
+import {filter, from, fromEvent} from "rxjs";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
   @Input() projects: any
 
   trackByData(index: number, projects: any): number {
     return projects.id;
   }
+
+  ngOnInit() {
+  }
+
 
   protected readonly ProjectSTATE = ProjectSTATE;
 }
